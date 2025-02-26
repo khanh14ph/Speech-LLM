fa1f75f59498d5063b45f0d55a3445944293d306
khanh14ph <khanh14ph@gmail.com>
2025-02-26 16:50:54 +0700

    ha

---
+++ finetune.py
@@ -0,0 +1,18 @@
+from transformers import MistralForCausalLM,BitsAndBytesConfig
+import torch
+base_model="Viet-Mistral/Vistral-7B-Chat"
+compute_dtype = getattr(torch, "float16")
+
+quant_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=False,
+)
+model = MistralForCausalLM.from_pretrained(
+    base_model,
+    quantization_config=quant_config,
+    device_map={"": 0}
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
